955b395042cfc79c26c9d49622478866cbc38088
Cephus Ongata <cephcroidson@gmail.com>
2026-08-24 17:53:04 +0300

    Add dark mode

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ const taskCounter = document.getElementById("taskCounter");
 const totalTasks = document.getElementById("totalTasks");
 const activeTasks = document.getElementById("activeTasks");
 const completedTasks = document.getElementById("completedTasks");
-
+const themeToggle = document.getElementById("themeToggle");
 const allFilter = document.getElementById("allFilter");
 const activeFilter = document.getElementById("activeFilter");
 const completedFilter = document.getElementById("completedFilter");
@@ -188,5 +188,29 @@ searchInput.addEventListener("input", () => {
 
     renderTasks();
 });
+function applyTheme() {
+    const savedTheme = localStorage.getItem("theme");
 
+    if (savedTheme === "dark") {
+        document.body.classList.add("dark-mode");
+        themeToggle.textContent = "☀️ Light Mode";
+    } else {
+        document.body.classList.remove("dark-mode");
+        themeToggle.textContent = "🌙 Dark Mode";
+    }
+}
+
+themeToggle.addEventListener("click", () => {
+    const isDark = document.body.classList.toggle("dark-mode");
+
+    if (isDark) {
+        localStorage.setItem("theme", "dark");
+        themeToggle.textContent = "☀️ Light Mode";
+    } else {
+        localStorage.setItem("theme", "light");
+        themeToggle.textContent = "🌙 Dark Mode";
+    }
+});
+
+applyTheme();
 renderTasks();
